5123394c518c7f408ea51ef6f52da4455102646e
jade <everydayfun@ukr.net>
2017-02-06 22:16:53 +0100

    Server methods send, process, broadcasting implemented
    method send - could send the message to one client at once
    broadcasting - sends to all the connected clients
    process - receives the messages

--- fooChatServer/src/main/java/Server.java
+++ fooChatServer/src/main/java/Server.java
@@ -1,12 +1,19 @@
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class Server implements Runnable {
 
+    private List<ClientData> clients = new ArrayList<ClientData>();
     private DatagramSocket socket;
     private Thread run, manage, send, receive;
+    private static AtomicLong idCounter = new AtomicLong();
+
     private int port;
     private boolean running;
 
@@ -40,6 +47,20 @@ public class Server implements Runnable {
         manage.start();
     }
 
+    private void send(final byte [] data, final InetAddress address, final int port){
+        send = new Thread("Send"){
+            public void run(){
+                DatagramPacket packet = new DatagramPacket(data, data.length, address, port);
+                try {
+                    socket.send(packet);
+                } catch (IOException e) {
+                    System.out.println("Doesnt sent to " + address + ":" + port);
+                }
+            }
+        };
+        send.start();
+    }
+
     public void receive(){
         receive = new Thread("Receive"){
              public void run(){
@@ -51,11 +72,36 @@ public class Server implements Runnable {
                      } catch (IOException e) {
                          e.printStackTrace();
                      }
-                     String str = new String(packet.getData());
-                     System.out.println(str);
+                     process(packet);
+                     clients.add(new ClientData("Eugene", createId(), packet.getPort(), packet.getAddress()));
+                     System.out.println(clients.get(0).address.toString() + ":" + clients.get(0).port); //do i need it?
                  }
              }
         };
         receive.start();
+    }
+
+    private void process(DatagramPacket packet){
+        String str = new String(packet.getData());
+        if (str.startsWith("/connect/")){
+            clients.add(new ClientData(str.substring(9,str.length()), createId(), packet.getPort(), packet.getAddress()));
+            System.out.println(str.substring(9,str.length())); //?? do i need it?
+        }else if (str.startsWith("/broadcast/")){
+            String message = str.substring(11,str.length());
+            broadcasting(message);
+        }else {
+            System.out.println(str);
+        }
+    }
+
+    private void broadcasting(String message){
+        for (int i = 0; i < clients.size(); i++) {
+            ClientData clientData = clients.get(i);
+            send(message.getBytes(), clientData.address, clientData.port);
+        }
+    }
+
+    private long createId(){
+        return idCounter.getAndIncrement();
     }
 }
